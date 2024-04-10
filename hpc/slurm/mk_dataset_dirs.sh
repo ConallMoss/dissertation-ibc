e49a2e00dc405fc639ff1091beacae16540b26da
Conall Moss <cm2121@login-p-4.data.cluster>
2024-04-10 21:04:16 +0100

    adding new datasets

--- hpc/slurm/mk_dataset_dirs.sh
+++ hpc/slurm/mk_dataset_dirs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-declare -a datasets=("facebook_combined" "slashdot-threads" "elec" "ego-facebook" "pajek-erdos" "email-EuAll" "epinions" "munmun_twitter_social")
+declare -a datasets=("amazon-ratings" "chess" "com-dblp" "dimacs9-NY" "ego-facebook" "elec" "email-EuAll" "epinions" "facebook-wosn-links" "github", "linux" "munmun_twitter_social" "pajek-erdos" "slashdot-threads" "sx-mathoverflow" "web-NotreDame" "youtube-groupmemberships" "facebook_combined")
 for i in "${datasets[@]}"
 do 
-    #rm -r "$i"
+    rm -rf "$i"
     mkdir -p "$i"
 done
